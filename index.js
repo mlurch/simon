665e9967833329f93c2837715c65bb6c7def059c
pearl <pearl@loertscher.org>
2023-03-23 14:35:01 -0600

    updated with db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const DB = require('./database.js');
 
 //service port, font end code is statically hosted at 3000
 const port = process.argv.length > 2 ? process.argv[2] : 3000; 
@@ -15,13 +16,15 @@ const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
 //GetScores
-apiRouter.get('/scores', (_req, res) => {
+apiRouter.get('/scores', async (_req, res) => {
+    const scores = await DB.getHighScores();
     res.send(scores);
 });
 
 //SubmitScore
-apiRouter.post('/score', (req, res) => {
-    scores = updateScores(req.body, scores);
+apiRouter.post('/score', async (req, res) => {
+    DB.addScore(req.body);
+    const scores = await DB.getHighScores();
     res.send(scores);
 });
 
@@ -33,29 +36,3 @@ app.use((_req, res) => {
 app.listen(port, () => {
     console.log(`Listening on port ${port}, bbg`);
 });
-
-//updateScores considers a new score for inclusion in the high scores
-//the high scores are saved in memory and disappear whenever the service closes
-let scores = [];
-function updateScores(newScore, scores) {
-    let found = false;
-    for (const [i, prevScore] of scores.entries()) {
-        if (newScore.score > prevScore.score) {
-            if (newScore.score > prevScore.score) {
-                scores.splice(i, 0, newScore);
-                found = true;
-                break;
-            }
-        }
-    }
-
-    if (!found) {
-        scores.push(newScore);
-    }
-
-    if (scores.length > 10) {
-        scores.length = 10;
-    }
-
-    return scores;
-}
