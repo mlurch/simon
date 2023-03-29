61bb16f5dad30eafe56772f2a7c2567e3bffe612
pearl <pearl@loertscher.org>
2023-03-29 14:14:51 -0600

    added websocket functionality

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
+const { PeerProxy } = require('./peerProxy.js');
 
 const authCookieName = 'token';
 
-// The service port. In production the application is statically hosted by the service on the same port.
+// The service port may be set on the command line
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
-//use the cookie parser middleware for tracking authentication tokens
+// Use the cookie parser middleware for tracking authentication tokens
 app.use(cookieParser());
 
 // Serve up the applications static content
@@ -22,14 +23,14 @@ app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-//createauth token for a new user
+// CreateAuth token for a new user
 apiRouter.post('/auth/create', async (req, res) => {
   if (await DB.getUser(req.body.email)) {
-    res.status(409).send({ msg: 'Existing user bozo'});
+    res.status(409).send({ msg: 'Existing user' });
   } else {
     const user = await DB.createUser(req.body.email, req.body.password);
 
-    //set the cookie
+    // Set the cookie
     setAuthCookie(res, user.token);
 
     res.send({
@@ -38,47 +39,47 @@ apiRouter.post('/auth/create', async (req, res) => {
   }
 });
 
-//getauth token for the provided credentials
+// GetAuth token for the provided credentials
 apiRouter.post('/auth/login', async (req, res) => {
   const user = await DB.getUser(req.body.email);
   if (user) {
     if (await bcrypt.compare(req.body.password, user.password)) {
       setAuthCookie(res, user.token);
-      res.send({ id: user._id});
+      res.send({ id: user._id });
       return;
     }
   }
-  res.status(401).send({ msg: 'Unauthorized bozo' });
+  res.status(401).send({ msg: 'Unauthorized' });
 });
 
-//deleteAuth token if stored in cookie
+// DeleteAuth token if stored in cookie
 apiRouter.delete('/auth/logout', (_req, res) => {
   res.clearCookie(authCookieName);
   res.status(204).end();
 });
 
-//getUser returns information about a user
+// GetUser returns information about a user
 apiRouter.get('/user/:email', async (req, res) => {
   const user = await DB.getUser(req.params.email);
   if (user) {
     const token = req?.cookies.token;
-    res.send({ email: user.email, authenticated: token === user.token});
+    res.send({ email: user.email, authenticated: token === user.token });
     return;
   }
-  res.status(404).send({ msg: 'Unknown bozo'});
+  res.status(404).send({ msg: 'Unknown' });
 });
 
-// secureApiRouter verifies credentials for endpounts
+// secureApiRouter verifies credentials for endpoints
 var secureApiRouter = express.Router();
 apiRouter.use(secureApiRouter);
 
 secureApiRouter.use(async (req, res, next) => {
-  authToken = req.cookies[authCookieName];
+  const authToken = req.cookies[authCookieName];
   const user = await DB.getUserByToken(authToken);
   if (user) {
     next();
   } else {
-    res.status(401).send({ msg: 'Unauthorized bozo'});
+    res.status(401).send({ msg: 'Unauthorized' });
   }
 });
 
@@ -95,17 +96,17 @@ secureApiRouter.post('/score', async (req, res) => {
   res.send(scores);
 });
 
-//default error handler
+// Default error handler
 app.use(function (err, req, res, next) {
   res.status(500).send({ type: err.name, message: err.message });
-}); 
+});
 
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
-//setAuthCookie in the HTTP response
+// setAuthCookie in the HTTP response
 function setAuthCookie(res, authToken) {
   res.cookie(authCookieName, authToken, {
     secure: true,
@@ -114,6 +115,8 @@ function setAuthCookie(res, authToken) {
   });
 }
 
-app.listen(port, () => {
+const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+new PeerProxy(httpService);
